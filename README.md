00b62f536cc5005f9f82287edaae8fcffd0cdbcf
Hong Tran <ttah.hongtran@gmail.com>
2023-08-04 10:51:47 +0700

    Update file

--- README.md
+++ README.md
@@ -1,14 +1,47 @@
 # WorldBank-Data-Dashboard
 
+This is a flask app that visualizes data from the world bank API. Data is pulled directly from the API and then visualized using Plotly.
+
 Develop and deploy a data dashboard as a web app using Bootstrap, Plotly, Flask and Heroku.
 
-Pull data directly from the [World Bank API](), clean the data in the back-end using pandas, and then display the results on your front-end. This would be instead of using a csv file for your data.
+Pull data directly from the [World Bank API](https://documents.worldbank.org/en/publication/documents-reports/api), clean the data in the back-end using pandas, and then display the results on your front-end. This would be instead of using a csv file for your data.
+
+## Installation
+
 
 ## Project description
 
 The main focus of this project is to practice how to pull data from an API, design a web app and deploy it to Heroku
 
-## APIs Besides the World Bank
-A few examples of public APIs include the [Twitter API](https://developer.twitter.com/en/docs), the [Google Maps API](https://mapsplatform.google.com/), the [Facebook Graph API](https://developers.facebook.com/docs/graph-api), and the [US Government Data APIs](https://data.gov/developers/apis/).
+## Process
+1. Pull data from [World Bank API](https://documents.worldbank.org/en/publication/documents-reports/api)
+2. Requirements.txt file containing the bare minimum package list/lists all of the Python packages that your app depends on.
 
+## How to run web app
+Pull this source code from github to your local machine and run the following commands in the project's root directory:
+
+```
+python myapp.py
+```
+
+To install the flask app, you need:
+
+- python3
+- python packages in the requirements.txt file
+
+Install the packages with
+```
+pip install -r requirements.txt
+```
+
+## Learn more:
+
+### APIs Besides the World Bank
+A few examples of public APIs include the [Twitter API](https://developer.twitter.com/en/docs), the [Google Maps API](https://mapsplatform.google.com/), the [Facebook Graph API](https://developers.facebook.com/docs/graph-api), and the [US Government Data APIs](https://data.gov/developers/apis/).
+You can pull data from these companies to create your own application.
+
+### Other Services Besides Heroku
+Heroku is just one option of many for deploying a web app, and Heroku is actually owned by [Salesforce](https://www.salesforce.com/ap/?ir=1).
+
+The big internet companies offer similar services like [Amazon's Lightsail](https://aws.amazon.com/lightsail/), [Microsoft's Azure](https://learn.microsoft.com/en-us/samples/azure-samples/python-docs-hello-world/python-flask-sample-for-azure-app-service-linux/), [Google Cloud](https://cloud.google.com/appengine/docs/legacy/standard/python/setting-up-environment), and [IBM Cloud](https://www.ibm.com/blog/) (formerly IBM Bluemix). However, these services tend to require more configuration. Most of these also come with either a free tier or a limited free tier that expires after a certain amount of time.
 
--- wrangling_scripts/wrangle_data.py
+++ wrangling_scripts/wrangle_data.py
@@ -1,8 +1,77 @@
 import pandas as pd
 import plotly.graph_objs as go
+import plotly.colors
+from collections import OrderedDict
+import requests
+import datetime
 
-# Use this file to read in your data and prepare the plotly visualizations. The path to the data files are in
-# `data/file_name.csv`
+def clean_data(current_year = datetime.date.today().year, last_n_year = 10):
+    '''
+    Pull data from World Bank Data API, clean and transform the data into desirable dataframe format 
+    to be ready for visualization with Plotly
+    '''
+
+    # Countries in SEA region
+    SEA_countries = OrderedDict([('Myanmar','MM'),('Singapore','SG'),('Laos','LA'),('Brunei','BN'),
+                                 ('Philippines','PH'),('Malaysia','MY'),('Cambodia','KH'),
+                                 ('Timor-Leste','TL'),('Thailand','TH'),('Indonesia','ID'),('Vietnam','VN')])
+    # Get all SEA countries and store as a string, separated by ;
+      ## Result: 'mm;sg;la;bn;ph;my;kh;tl;th;id;vn'
+    SEA_countries_str = list(SEA_countries.values())
+    SEA_countries_str = [x.lower() for x in SEA_countries_str]
+    SEA_countries_str = ';'.join(SEA_countries_str)
+
+    # World Bank indicators of interest
+    indicators = {'Population, total':'SP.POP.TOTL',
+                  'GDP per capita (current US$)':'NY.GDP.PCAP.CD',
+                  'Unemployment, total (% of total labor force) (national estimate)':'SL.UEM.TOTL.NE.ZS',
+                  'Foreign direct investment, net inflows (BoP, current US$)':'BX.KLT.DINV.CD.WD'}
+    
+    # URL endpoints for the World Bank API
+    urls = []
+    # Pass a dict of value as parameters to the url instead of specifying the entire URL in a single string
+    payload = {'format': 'json', 'per_page': '1000', 'date':'{}:{}'.format(current_year-last_n_year, current_year)}
+    # Store the data frames with the indicator data of interest
+    data_frames = []
+    
+    # Pull data from World Bank API in json format and store in a dataframe
+    for ind in indicators.values():
+        url = 'http://api.worldbank.org/v2/countries/' + SEA_countries_str +'/indicators/' + ind
+        urls.append(url)
+        try:
+            r = requests.get(url, params=payload)
+            data = r.json()[1]
+        except:
+            print('Data not found for indicator: ', ind)
+ 
+        for i, val in enumerate(data):
+            val['indicator'] = val['indicator']['value']
+            val['country'] = val['country']['value']
+
+        data_frames.append(data)
+
+    # create an Empty DataFrame object
+    df = pd.DataFrame()
+    # Append data to final dataframe
+    for i in range(len(data_frames)):
+        df_new = pd.DataFrame(data_frames[i])
+        df = pd.concat([df,df_new])
+
+    # Select columns of interest
+    df = df[['indicator','country','date','value']]
+
+    # Rename value
+    df['indicator'] = df['indicator'].replace('Population, total','Total population')
+    df['indicator'] = df['indicator'].replace('Unemployment, total (% of total labor force) (national estimate)','Total unemployment (%)')
+    df['indicator'] = df['indicator'].replace('Foreign direct investment, net inflows (BoP, current US$)','FDI')
+
+
+    # Data type
+    df['date'] = df['date'].astype('str')
+    df['value'] = df['value'].astype('float64')
+    
+    return df
+
 
 def return_figures():
     """Creates four plotly visualizations
@@ -14,70 +83,65 @@ def return_figures():
         list (dict): list containing the four plotly visualizations
 
     """
+    df = clean_data()
+    list_countries = df['country'].unique().tolist()
+    last_n_year = len(df['date'].unique())
 
-    # first chart plots arable land from 1990 to 2015 in top 10 economies 
-    # as a line chart
-    
-    graph_one = []    
-    graph_one.append(
-      go.Scatter(
-      x = [0, 1, 2, 3, 4, 5],
-      y = [0, 2, 4, 6, 8, 10],
-      mode = 'lines'
-      )
-    )
+    # graph one
+    graph_one = []
+    for country in list_countries:
+        df_graph_1 = df.loc[(df['country']==country)&(df['indicator']=='Total population')]
+        x_val = df_graph_1['date'].tolist()
+        y_val = df_graph_1['value'].tolist()
+        graph_one.append(go.Scatter(x = x_val,
+                                    y = y_val,
+                                    mode = 'lines',
+                                    name = country))
+    layout_one = dict(title = 'Total population',
+                      xaxis = dict(title = 'Year'),
+                      yaxis = dict(title = 'Population'))
 
-    layout_one = dict(title = 'Chart One',
-                xaxis = dict(title = 'x-axis label'),
-                yaxis = dict(title = 'y-axis label'),
-                )
-
-# second chart plots ararble land for 2015 as a bar chart    
+    # graph two
     graph_two = []
-
-    graph_two.append(
-      go.Bar(
-      x = ['a', 'b', 'c', 'd', 'e'],
-      y = [12, 9, 7, 5, 1],
-      )
-    )
-
-    layout_two = dict(title = 'Chart Two',
-                xaxis = dict(title = 'x-axis label',),
-                yaxis = dict(title = 'y-axis label'),
-                )
-
+    for country in list_countries:
+        df_graph_2 = df.loc[(df['country']==country)&(df['indicator']=='GDP per capita')]
+        x_val = df_graph_2['date'].tolist()
+        y_val = df_graph_2['value'].tolist()
+        graph_two.append(go.Scatter(x = x_val,
+                                    y = y_val,
+                                    mode = 'lines',
+                                    name = country))
+    layout_two = dict(title = 'GDP per capita (current US$)',
+                      xaxis = dict(title = 'Year'),
+                      yaxis = dict(title = 'USD'))
 
 # third chart plots percent of population that is rural from 1990 to 2015
     graph_three = []
-    graph_three.append(
-      go.Scatter(
-      x = [5, 4, 3, 2, 1, 0],
-      y = [0, 2, 4, 6, 8, 10],
-      mode = 'lines'
-      )
-    )
-
-    layout_three = dict(title = 'Chart Three',
-                xaxis = dict(title = 'x-axis label'),
-                yaxis = dict(title = 'y-axis label')
-                       )
+    for country in list_countries:
+        df_graph_3 = df.loc[(df['country']==country)&(df['indicator']=='Total unemployment (%)')]
+        x_val = df_graph_3['date'].tolist()
+        y_val = df_graph_3['value'].tolist()
+        graph_three.append(go.Scatter(x = x_val,
+                                      y = y_val,
+                                      mode = 'lines',
+                                      name = country))
+    layout_three = dict(title = 'Total unemployment (%)',
+                        xaxis = dict(title = 'Year'),
+                        yaxis = dict(title = '% of Total labor force'))
     
 # fourth chart shows rural population vs arable land
     graph_four = []
-    
-    graph_four.append(
-      go.Scatter(
-      x = [20, 40, 60, 80],
-      y = [10, 20, 30, 40],
-      mode = 'markers'
-      )
-    )
-
-    layout_four = dict(title = 'Chart Four',
-                xaxis = dict(title = 'x-axis label'),
-                yaxis = dict(title = 'y-axis label'),
-                )
+    for country in list_countries:
+        df_graph_4 = df.loc[(df['country']==country)&(df['indicator']=='FDI')]
+        x_val = df_graph_4['date'].tolist()
+        y_val = df_graph_4['value'].tolist()
+        graph_four.append(go.Scatter(x = x_val,
+                                     y = y_val,
+                                     mode = 'lines',
+                                     name = country))
+    layout_four = dict(title = 'Foreign direct investment (FDI), net inflows',
+                       xaxis = dict(title = 'Year'),
+                       yaxis = dict(title = 'USD'))
     
     # append all charts to the figures list
     figures = []
